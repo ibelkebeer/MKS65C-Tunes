06c0abea589714e51495f660b0f99eb5b987f9e3
Imad <ibelkebir@stuy.edu>
2018-10-17 00:21:11 -0400

    fixed .h problem

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,6 @@
+#ifndef ll
+#define ll
+
 struct node {char name[100]; char artist[100]; struct node* next;};
 void print_list(struct node* cur);
 struct node* insert_front(struct node* cur, char name[], char artist[]);
@@ -7,3 +10,5 @@ struct node* del(struct node* cur, char name[], char artist[]);
 struct node* find_artist(struct node* cur, char artist[]);
 struct node* find_song(struct node* cur, char name[], char artist[]);
 void print_node(struct node* cur);
+
+#endif
